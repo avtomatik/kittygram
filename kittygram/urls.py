2eb808a6427919c48a1c473617b3c35d4cf97285
Alexander <kkruscherr@gmail.com>
2024-07-07 23:08:49 +0300

    Rewrote with `ModelViewSet`

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,12 @@
-# Обновлённый urls.py
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
 
-from cats.views import CatDetail, CatList
+from cats.views import CatViewSet
+
+router = SimpleRouter()
+
+router.register('cats', CatViewSet)
 
 urlpatterns = [
-    path('cats/', CatList.as_view()),
-    path('cats/<int:pk>/', CatDetail.as_view()),
+    path('', include(router.urls))
 ]
