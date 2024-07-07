07f6dc788ab608d33f68f3baac2c6e04ccf7620b
Alexander <kkruscherr@gmail.com>
2024-07-07 20:29:21 +0300

    Rewrote Based on Generic Views

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,7 +1,9 @@
+# Обновлённый urls.py
 from django.urls import path
 
-from cats.views import APICat
+from cats.views import CatDetail, CatList
 
 urlpatterns = [
-    path('cats/', APICat.as_view()),
+    path('cats/', CatList.as_view()),
+    path('cats/<int:pk>/', CatDetail.as_view()),
 ]
